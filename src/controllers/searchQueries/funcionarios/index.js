6f931c72593131eb226f73a17140e7e9bed57e58
João Pedro <jopdaltro@gmail.com>
2023-10-24 02:03:39 -0300

    adicionando routes para funcionarios

--- src/controllers/searchQueries/funcionarios/index.js
+++ src/controllers/searchQueries/funcionarios/index.js
@@ -17,8 +17,9 @@ class Funcionario {
     }
     
     async listarFuncionarioIdade(req, res) {
-        if (req.body.idade !== undefined) { // Certifique-se de verificar a chave correta no corpo da solicitação, como "idade".
-            let idade = req.body.idade;
+        const {idade} = req.query
+        if (idade !== undefined) { // Certifique-se de verificar a chave correta no corpo da solicitação, como "idade".
+            //let idade = req.body.idade;
     
             db.query(
                 "SELECT * FROM funcionario WHERE YEAR(CURDATE()) - YEAR(data_nasc) >= ?",
@@ -42,8 +43,8 @@ class Funcionario {
     }
 
     async listarFuncionarioDataIngresso(req, res) {
-        if (req.body.dataIngresso !== undefined) { 
-            let dataIngresso = req.body.dataIngresso;
+        const {dataIngresso} = req.query
+        if (dataIngresso !== undefined) { 
             
     
             db.query(
@@ -69,11 +70,13 @@ class Funcionario {
     
     
     async listarFuncionarioEndereco(req, res) {
-        if (req.body.cidade !== undefined && req.body.bairro !== undefined && req.body.rua !== undefined && req.body.numero !== undefined) {
-            let cidade = req.body.cidade;
-            let bairro = req.body.bairro;
-            let rua = req.body.rua;
-            let numero = req.body.numero;
+        // /funcionaraio?cidade=Cruz da Almas&rua=tabela&bairro=centro&numero=10
+        const {cidade, rua, bairro, numero} = req.query
+        if (cidade !== undefined && bairro !== undefined && rua !== undefined && numero !== undefined) {
+            // let cidade = req.body.cidade;
+            // let bairro = req.body.bairro;
+            // let rua = req.body.rua;
+            // let numero = req.body.numero;
             
     
             db.query(
@@ -100,40 +103,40 @@ class Funcionario {
     }
     
     async listarFuncionarioCargo(req, res) {
-    if (req.body.cargoNome !== undefined) { 
-        let cargoNome = req.body.cargoNome;
-        
 
-        db.query(
-            "SELECT f.* FROM funcionario AS f " +
-            "JOIN cargo AS c ON f.cargo_id = c.id " +
-            "WHERE c.nome = ?",
-            [cargoNome],
-            function (err, results) {
-                if (err) {
-                    console.log(err);
-                    res.status(500).json({ data: "Erro Interno do Servidor" });
-                } else {
-                    if (results.length > 0) {
-                        res.status(200).json({ data: results });
+        const {cargoNome} = req.query
+        console.log(cargoNome)
+        if (cargoNome !== undefined) { 
+            // let cargoNome = req.body.cargoNome;
+
+            const sql = `
+            SELECT f.* 
+                FROM funcionario AS 
+                    f JOIN cargo AS 
+                        c ON f.cargo_id = c.id 
+                            WHERE c.nome = ?
+            `
+            
+
+            db.query(
+                sql,
+                [cargoNome],
+                function (err, results) {
+                    if (err) {
+                        console.log(err);
+                        res.status(500).json({ data: "Erro Interno do Servidor" });
                     } else {
-                        res.status(404).json({ data: "Nenhum Funcionário Encontrado com esse cargo" });
+                        if (results.length > 0) {
+                            res.status(200).json({ data: results });
+                        } else {
+                            res.status(404).json({ data: "Nenhum Funcionário Encontrado com esse cargo" });
+                        }
                     }
                 }
-            }
-        );
-    } else {
-        return res.status(400).json({ data: "Informações Recebidas Inválidas" });
+            );
+        } else {
+            return res.status(400).json({ data: "Informações Recebidas Inválidas" });
+        }
     }
 }
-
-
-
-
-
-
-
-
-}
-
 module.exports = new Funcionario;
