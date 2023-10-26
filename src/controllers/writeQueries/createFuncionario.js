e69483154e7dbf4588783caa4500306615b60503
Guilhermeugenio <38566707+guilhermeugenio@users.noreply.github.com>
2023-10-26 00:45:36 -0300

    Corrigindo erro da branch

--- src/controllers/writeQueries/createFuncionario.js
+++ src/controllers/writeQueries/createFuncionario.js
@@ -1,92 +1,89 @@
+const db = require('../../database/connect');
 
+class Funcionario {
+  async createFuncionario(req, res) {
+    const { nome, cpf, email, senha, sexo, estado_civil, data_ingresso, telefone, id_endereco, id_cargo } = req.body;
 
-const bb = require('../../database/connect')
+    // Verifica se o endereço existe
+    const enderecoSql = `
+      SELECT * FROM endereco WHERE id = ?;
+    `;
+    db.query(enderecoSql, [id_endereco], (err, enderecoResult) => {
+      if (err) {
+        return res.status(500).json({ message: 'Erro ao verificar o endereço do funcionário' });
+      }
 
-class Funcionario{
-    async createFuncionario(req, res) {
-        const { nome, cpf, email, senha, sexo, estado_civil, data_ingresso, telefone, id_endereco, id_cargo } = req.body;
-      
-        // Verifica se o endereço existe
-        const enderecoSql = `
-          SELECT * FROM endereco WHERE id = ?;
+      if (enderecoResult.length === 0) {
+        return res.status(400).json({ message: 'Endereço não encontrado' });
+      }
+
+      // Verifica se o cargo existe
+      const cargoSql = `
+        SELECT * FROM cargo WHERE id = ?;
+      `;
+      db.query(cargoSql, [id_cargo], (err, cargoResult) => {
+        if (err) {
+          return res.status(500).json({ message: 'Erro ao verificar o cargo do funcionário' });
+        }
+
+        if (cargoResult.length === 0) {
+          return res.status(400).json({ message: 'Cargo não encontrado' });
+        }
+
+        // Insere o funcionário
+        const sql = `
+          INSERT INTO funcionario (
+            nome, cpf, email, senha, sexo, estado_civil, data_ingresso, telefone, id_endereco, id_cargo
+          ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
         `;
-        db.query(enderecoSql, [id_endereco], (err, enderecoResult) => {
+        db.query(sql, [nome, cpf, email, senha, sexo, estado_civil, data_ingresso, telefone, id_endereco, id_cargo], (err, result) => {
           if (err) {
-            return res.status(500).json({ message: 'Erro ao verificar o endereço do funcionário' });
+            return res.status(500).json({ message: 'Erro ao inserir o funcionário no banco de dados' });
           }
-      
-          if (enderecoResult.length === 0) {
-            return res.status(400).json({ message: 'Endereço não encontrado' });
-          }
-      
-          // Verifica se o cargo existe
-          const cargoSql = `
-            SELECT * FROM cargo WHERE id = ?;
+
+          // Consulta para obter os dados do funcionário recém-inserido
+          const funcionarioId = result.insertId;
+          const selectSql = `
+            SELECT f.*, e.cidade, e.bairro, e.rua, e.numero, c.nome AS nome_cargo
+            FROM funcionario f
+            LEFT JOIN endereco e ON f.id_endereco = e.id
+            LEFT JOIN cargo c ON f.id_cargo = c.id
+            WHERE f.id = ?;
           `;
-          db.query(cargoSql, [id_cargo], (err, cargoResult) => {
+          db.query(selectSql, [funcionarioId], (err, funcionario) => {
             if (err) {
-              return res.status(500).json({ message: 'Erro ao verificar o cargo do funcionário' });
+              return res.status(500).json({ message: 'Erro ao obter os dados do funcionário' });
             }
-      
-            if (cargoResult.length === 0) {
-              return res.status(400).json({ message: 'Cargo não encontrado' });
-            }
-      
-            // Insere o funcionário
-            const sql = `
-              INSERT INTO funcionario (
-                nome, cpf, email, senha, sexo, estado_civil, data_ingresso, telefone, id_endereco, id_cargo
-              ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-            `;
-            db.query(sql, [nome, cpf, email, senha, sexo, estado_civil, data_ingresso, telefone, id_endereco, id_cargo], (err, result) => {
-              if (err) {
-                return res.status(500).json({ message: 'Erro ao inserir o funcionário no banco de dados' });
-              }
-      
-              // Consulta para obter os dados do funcionário recém-inserido
-              const funcionarioId = result.insertId;
-              const selectSql = `
-                SELECT f.*, e.cidade, e.bairro, e.rua, e.numero, c.nome AS nome_cargo
-                FROM funcionario f
-                LEFT JOIN endereco e ON f.id_endereco = e.id
-                LEFT JOIN cargo c ON f.id_cargo = c.id
-                WHERE f.id = ?;
-              `;
-              db.query(selectSql, [funcionarioId], (err, funcionario) => {
-                if (err) {
-                  return res.status(500).json({ message: 'Erro ao obter os dados do funcionário' });
-                }
-      
-                const funcionarioData = {
-                  id: funcionario[0].id,
-                  nome: funcionario[0].nome,
-                  cpf: funcionario[0].cpf,
-                  email: funcionario[0].email,
-                  senha: funcionario[0].senha,
-                  sexo: funcionario[0].sexo,
-                  estado_civil: funcionario[0].estado_civil,
-                  data_ingresso: funcionario[0].data_ingresso,
-                  telefone: funcionario[0].telefone,
-                  endereco: {
-                    id: funcionario[0].id_endereco,
-                    cidade: funcionario[0].cidade,
-                    bairro: funcionario[0].bairro,
-                    rua: funcionario[0].rua,
-                    numero: funcionario[0].numero,
-                  },
-                  cargo: {
-                    id: funcionario[0].id_cargo,
-                    nome: funcionario[0].nome_cargo,
-                  },
-                };
-      
-                return res.status(201).json({ message: 'Funcionário cadastrado com sucesso', funcionario: funcionarioData });
-              });
-      
 
+            const funcionarioData = {
+              id: funcionario[0].id,
+              nome: funcionario[0].nome,
+              cpf: funcionario[0].cpf,
+              email: funcionario[0].email,
+              senha: funcionario[0].senha,
+              sexo: funcionario[0].sexo,
+              estado_civil: funcionario[0].estado_civil,
+              data_ingresso: funcionario[0].data_ingresso,
+              telefone: funcionario[0].telefone,
+              endereco: {
+                id: funcionario[0].id_endereco,
+                cidade: funcionario[0].cidade,
+                bairro: funcionario[0].bairro,
+                rua: funcionario[0].rua,
+                numero: funcionario[0].numero,
+              },
+              cargo: {
+                id: funcionario[0].id_cargo,
+                nome: funcionario[0].nome_cargo,
+              },
+            };
 
-    
-
+            return res.status(201).json({ message: 'Funcionário cadastrado com sucesso', funcionario: funcionarioData });
+          });
+        });
+      });
+    });
+  }
 }
 
-module.exports = new Funcionario
+module.exports = new Funcionario();
